2f9e87057efc86187f30d2771f1b4d8b806a437f
Dimitri Racordon <dimitri.racordon@gmail.com>
2018-09-19 17:09:38 +0200

    Better constraint the abstract types of the PetriNet protocol

--- Sources/PetriKit/PetriKit.swift
+++ Sources/PetriKit/PetriKit.swift
@@ -1,28 +1,27 @@
 public protocol PetriNet {
-  
-  associatedtype PlaceType: Hashable
-  associatedtype TransitionType: Transition
-  
+
   associatedtype MarkingType
-  
+  associatedtype PlaceType: Hashable
+  associatedtype TransitionType: Transition where TransitionType.MarkingType == MarkingType
+
   var places     : Set<PlaceType>      { get }
   var transitions: Set<TransitionType> { get }
-  
+
   func simulate(steps: Int, from: MarkingType) -> MarkingType
-  
+
 }
 
 // ---------------------------------------------------------------------------
 
 public protocol Transition: Hashable {
-  
+
   associatedtype ArcType: Hashable
   associatedtype MarkingType
-  
+
   var preconditions : Set<ArcType> { get }
   var postconditions: Set<ArcType> { get }
-  
+
   func isFireable(from marking: MarkingType) -> Bool
   func fire(from marking: MarkingType) -> MarkingType?
-  
+
 }
