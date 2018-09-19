61362a3c76d0161d3606c0b22777d2e639503511
Dimitri Racordon <dimitri.racordon@gmail.com>
2018-09-19 19:21:25 +0200

    Add test for the Marking type

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,6 +2,7 @@ import XCTest
 @testable import PetriKitTests
 
 XCTMain([
+  testCase(MarkingTests.allTests),
   testCase(PetriKitTests.allTests),
   testCase(PTNetTests.allTests),
   testCase(RandomTests.allTests),
