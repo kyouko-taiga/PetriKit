faa5cc67fb562e62ab882151861561a457ccd01c
Dimitri Racordon <dimitri.racordon@gmail.com>
2018-09-19 16:25:27 +0200

    Update Package.swift to the Swift's 4 format

--- Package.swift
+++ Package.swift
@@ -1,7 +1,15 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.2
 
 import PackageDescription
 
 
 let package = Package(
-    name: "PetriKit")
+    name: "PetriKit",
+    products: [
+      .library(name: "PetriKit", type: .static, targets: ["PetriKit"]),
+    ],
+    targets: [
+      .target(name: "PetriKit"),
+      .testTarget(name: "PetriKitTests"),
+    ]
+  )
